5c5829c61bf9f53ca766fa7a5a448c44df2d6759
danza13 <136966054+danza13@users.noreply.github.com>
2025-04-16 22:41:19 +0300

    Update bot_start.py

--- bot_start.py
+++ bot_start.py
@@ -108,7 +108,7 @@ async def handle_webapp(message: types.Message, state: FSMContext):
     kb.add(KeyboardButton("Далі"))
     kb.add(KeyboardButton("Скасувати"))
     await message.answer(
-        f"Перевірте дані:\n🏠 {place}\n🕒 {dt_str}\n👥 {guests}\n📝 {name}\n\n"
+        f"Перевірте дані:\n🏠 Заклад: {place}\n🕒 Час та дата: {dt_str}\n👥 Кількість гостей: {guests}\n📝 Контактна особа: {name}\n\n"
         "Якщо все вірно — натисніть «Далі».",
         reply_markup=kb)
     await BookingStates.CONFIRM_DATA.set()
